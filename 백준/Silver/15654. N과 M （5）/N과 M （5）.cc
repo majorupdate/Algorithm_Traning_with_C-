e69d7db9b02e2055bcce7000ac93965a4a61f0f6
Hong <117250328+majorupdate@users.noreply.github.com>
2023-02-06 13:04:46 +0900

    [Silver III] Title: N과 M (5), Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int list[8] = {0};
+int arr[8] = {0};
+bool check[8] = {false};
+
+void dfs(int cnt, int n, int m)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (check[i] == true)   
+        {
+            continue;
+        }
+        check[i] = true;
+        arr[cnt] = list[i];
+        dfs(cnt + 1, n, m);
+        check[i] = false;
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> list[i];
+    }
+    sort(list, list + n);
+    
+    dfs(0, n, m);
+
+    return 0;
+}
