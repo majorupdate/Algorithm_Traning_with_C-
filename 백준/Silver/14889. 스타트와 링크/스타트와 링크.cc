1ff0115eaec1fdf863d0b9c6e3d23b1bbdc5a7eb
Hong <117250328+majorupdate@users.noreply.github.com>
2023-02-07 11:59:50 +0900

    [Silver II] Title: 스타트와 링크, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -5,32 +5,38 @@
 
 using namespace std;
 
-bool check[10] = {false};
+bool check[20] = {false};
+int list_1[10] = {0};
+int list_2[10] = {0};
 int s[20][20] = {0};
 
-int ans = 1000000000;
+vector<int> v;
 
 void dfs(int cnt, int n, int a)
 {
     if (cnt == n / 2)
     {
-        int sum1 = 0, sum2 = 0;
-        for (int i = 0; i < n; i++)
+        int cnt_2 = 0;
+        for (int i = 1; i < n; i++)
         {
-            for (int j = 0; j < n; j++)
+            if (check[i] == false)
             {
-                if (check[i] == true && check[j] == true)
-                {
-                    sum1 += s[i][j];
-                }
-                if (check[i] == false && check[j] == false)
-                {
-                    sum2 += s[i][j];
-                }
+                list_2[cnt_2++] = i;
             }
         }
-
-        ans = min(ans, abs(sum1 - sum2));
+        
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < cnt - 1; i++)
+        {
+            for (int j = i + 1; j < cnt; j++)
+            {
+                sum1 += (s[list_1[i]][list_1[j]] + s[list_1[j]][list_1[i]]);
+                sum2 += (s[list_2[i]][list_2[j]] + s[list_2[j]][list_2[i]]);
+            }
+        }
+        
+        v.push_back(abs(sum1 - sum2));
+        return;
     }
 
     for (int i = a; i < n; i++)
@@ -40,15 +46,17 @@ void dfs(int cnt, int n, int a)
             continue;
         }
         check[i] = true;
-        dfs(cnt + 1, n, i + 1);
+        list_1[cnt] = i;
+        dfs(cnt + 1, n, i);
         check[i] = false;
     }
 }
 
 int main()
 {
-    int n; cin >> n;
-    
+    int n;
+    cin >> n;
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -56,11 +64,13 @@ int main()
             cin >> s[i][j];
         }
     }
-    
+
+    list_1[0] = 0;
     check[0] = true;
     dfs(1, n, 1);
-    
-    cout << ans;
+
+    int min = *min_element(v.begin(), v.end());
+    cout << min;
 
     return 0;
 }
